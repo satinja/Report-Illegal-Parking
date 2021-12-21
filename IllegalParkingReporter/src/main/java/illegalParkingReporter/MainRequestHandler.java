dec21a2bedcba079357843c68bb124f42d53ecf2
Satinjal Yadav <satin.yadav4@gmail.com>
2021-12-21 19:45:49 +0530

    Created a zero level of application

---
+++ IllegalParkingReporter/src/main/java/illegalParkingReporter/MainRequestHandler.java
@@ -0,0 +1,33 @@
+package illegalParkingReporter;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainRequestHandler
+ */
+@WebServlet(description = "This class is used for handling incoming requests and to process them.", urlPatterns = { "/MainRequestHandler" })
+public class MainRequestHandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
