fbc72bb129da44637d8171dcb76d7810696ec2bb
unknown <ykanish>
2021-12-22 16:52:37 +0530

    Created Login Handler, added ajax call in JAVA SCRIPT

--- IllegalParkingReporter/src/main/java/login/LoginDetails.java
+++ IllegalParkingReporter/src/main/java/login/LoginDetails.java
@@ -1,4 +1,4 @@
-package illegalParkingReporter;
+package login;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,18 +8,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MainRequestHandler
+ * Servlet implementation class LoginDetails
  */
-@WebServlet(description = "This class is used for handling incoming requests and to process them.", urlPatterns = { "/MainRequestHandler" })
-public class MainRequestHandler extends HttpServlet {
+@WebServlet("/DoLogin")
+public class LoginDetails extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: doGet ").append(request.getContextPath());
+		if(request.getParameter("email_id").equals("yadavkanishk57@gmail.com")  &&
+		   request.getParameter("password").equals("Kanishk@123"))
+		response.getWriter().append("Served at: Success ").append(request.getContextPath());
+		else
+			response.getWriter().append("Served at: Request Denied!").append(request.getContextPath());
 	}
 
 	/**
@@ -27,7 +39,7 @@ public class MainRequestHandler extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at:doPost ").append(request.getContextPath());
+		doGet(request, response);
 	}
 
 }
